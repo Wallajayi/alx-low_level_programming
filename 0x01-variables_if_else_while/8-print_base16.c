ffd9850274f9f6ab028219b0e4c85175c9843989
Wallajayi <ashaliter2@gmail.com>
2023-02-20 15:39:02 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,8 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = '0' ; i < 10 ; i++)
-		putchar((i % 10) + '0');
+	for (i = '0' ; i <= '9' ; i++)
+		putchar('0' + i);
 	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
