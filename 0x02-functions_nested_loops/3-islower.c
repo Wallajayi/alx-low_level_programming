190657be15973c3dad5ef7483811cae913285cdc
Wallajayi <ashaliter2@gmail.com>
2023-02-21 14:36:25 -0800

    Task 3 correctes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * main - entry
- *_islower - functions to print lowercase
+ * _islower - functions to print lowercase
  *@c: character to print
  * Return: 1 if its a lowercase and 0 otherwise
  */
+
 int _islower(int c)
 {
-	if (c >= 97 && c >= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
