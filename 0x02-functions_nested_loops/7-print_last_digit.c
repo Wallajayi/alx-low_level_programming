9499080cf73c3dbf2db58c2e75ef18eb3f7e2906
Wallajayi <ashaliter2@gmail.com>
2023-02-21 16:19:15 -0800

    Task 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: "Function that prints last digit"
+ *
+ * @i: parameter to be checked
+ * Return: k
+ */
+
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
